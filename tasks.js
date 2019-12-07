b413d64001f448e7164bb76d90436807dfb3be7c
Joanna Skrzypińska <jm.skrzypinska@gmail.com>
2019-12-07 12:20:14 +0100

    changes in tasks.js

--- tasks.js
+++ tasks.js
@@ -1,24 +1,25 @@
 const url = "http://localhost:4000/api/tasks";
 
-const form = document.getElementById("add-task-form");
-const inputTask = document.getElementById("input-task");
-document.getElementById("btn-add-task").addEventListener("click", addTask);
+const form1 = document.getElementById("add-task-form1");
+const inputTask1 = document.getElementById("input-task1");
+document.getElementById("btn-add-task1").addEventListener("click", addTask);
 
 let tasks = [];
 
 function addTask(e) {
-  if (inputTask.validity.valid) {
+  if (inputTask1.validity.valid) {
     //let task = document.getElementById("input-task").value;
-    // console.log(e.current.value);
-    tasks.push(inputTask.value);
-    let ul = document.getElementById("list");
-    let li = document.createElement("li");
-    li.className = "list-group-item";
-    li.innerHTML = inputTask.value;
-    li.setAttribute("id", "task");
-    ul.appendChild(li);
+    console.log(e.target.value);
+    tasks.push(inputTask1.value);
+    let ul1 = document.getElementById("list1");
+    let li1 = document.createElement("li");
+    li1.className = "list-group-item";
+    li1.innerHTML = `${inputTask1.value} <span class="badge badge-danger">X</span><span class="badge badge-warning">In Progress</span>
+    <span class="badge badge-success">Done</span>`;
+    li1.setAttribute("id", "task");
+    ul1.appendChild(li1);
     // alert(li.id);
-    inputTask.value = "";
+    inputTask1.value = "";
     e.preventDefault();
   }
 }
